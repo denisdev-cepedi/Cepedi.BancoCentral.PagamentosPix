11219d6b425c9c44fe3796d0b10bf32033327cc2
Ana Caroline <carolnerisfonseca@gmail.com>
2024-04-11 15:15:06 -0300

    create pessoa

--- src/Cepedi.BancoCentral.PagamentoPix.Domain/Entities/PessoaEntity.cs
+++ src/Cepedi.BancoCentral.PagamentoPix.Domain/Entities/PessoaEntity.cs
@@ -1,5 +1,9 @@
-namespace Namespace;
-public class Class
-{
+namespace Cepedi.BancoCentral.PagamentoPix.Domain.Entities;
 
+public class PessoaEntity
+{
+    public int IdPessoa { get; set; }
+    public required string Nome { get; set; }
+    public required string Cpf { get; set; }
+    public required string IdConta { get; set; }
 }
