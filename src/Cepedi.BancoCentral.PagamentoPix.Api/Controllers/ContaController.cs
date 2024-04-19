608c40148048b05fab283147ee16dd54a3e1babd
Carol <carolnerisfonseca@gmail.com>
2024-04-18 21:51:58 -0300

    create controller conta

---
+++ src/Cepedi.BancoCentral.PagamentoPix.Api/Controllers/ContaController.cs
@@ -0,0 +1,46 @@
+﻿using Cepedi.BancoCentral.PagamentoPix.Compartilhado;
+using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Excecoes;
+using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Requests;
+using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cepedi.BancoCentral.PagamentoPix.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ContaController : BaseController
+{
+    private readonly ILogger<ContaController> _logger;
+    private readonly IMediator _mediator;
+
+    public ContaController(
+        ILogger<ContaController> logger, IMediator mediator)
+        : base(mediator)
+    {
+        _logger = logger;
+        _mediator = mediator;
+    }
+
+
+    [HttpPost]
+    [ProducesResponseType(typeof(CriarContaResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<CriarContaResponse>> CriarContaAsync(
+        [FromBody] CriarContaResquest request) => await SendCommand(request);
+
+    private async Task<ActionResult<CriarContaResponse>> SendCommand(CriarContaResquest request)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+    /*[HttpPut]
+    [ProducesResponseType(typeof(AtualizarContaResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status204NoContent)]
+    public async Task<ActionResult<AtualizarContaResponse>> AtualizarContaAsync(
+        [FromBody] AtualizarContaRequest request) => await SendCommand(request);
+*/
+}
