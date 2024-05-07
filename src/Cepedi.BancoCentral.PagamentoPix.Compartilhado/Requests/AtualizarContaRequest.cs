31be690b45ffb626af933fdc77bf102341885c8f
Lorena Andrade <lorenaandrade@gmail.com>
2024-05-07 07:20:36 -0300

    detalhes

--- src/Cepedi.BancoCentral.PagamentoPix.Compartilhado/Requests/AtualizarContaRequest.cs
+++ src/Cepedi.BancoCentral.PagamentoPix.Compartilhado/Requests/AtualizarContaRequest.cs
@@ -1,5 +1,4 @@
-﻿using Cepedi.BancoCentral.PagamentoPix.Compartilhado;
-using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Responses;
+﻿using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Responses;
 using MediatR;
 using OperationResult;
 namespace Cepedi.BancoCentral.PagamentoPix.Compartilhado.Requests;
