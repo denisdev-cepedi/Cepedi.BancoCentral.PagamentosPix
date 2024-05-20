df713efea7e3ddccd3e7772aa2953d596b2892c5
Denis Vieira <denisvieira@apostaganha.bet>
2024-05-20 16:22:16 -0300

    Adicionando cache

---
+++ src/Cepedi.BancoCentral.PagamentoPix.Dados/Repositorios/Queries/PessoaQueryRepository.cs
@@ -0,0 +1,32 @@
+﻿using Cepedi.BancoCentral.PagamentoPix.Dominio.Entidades;
+using Cepedi.BancoCentral.PagamentoPix.Dominio.Repositorio.Queries;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Cepedi.BancoCentral.PagamentoPix.Dados.Repositorios.Queries;
+public class PessoaQueryRepository : BaseDapperRepository, IPessoaQueryRepository
+{
+    public PessoaQueryRepository(IConfiguration configuration) 
+        : base(configuration)
+    {
+    }
+
+    public async Task<List<PessoaEntity>> ObterPessoasAsync(string nome)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@Nome", nome, System.Data.DbType.String);
+
+        var sql = @"SELECT 
+                        IdPessoa, 
+                        Nome,
+                        Cpf
+                    FROM Pessoa WITH(NOLOCK)
+                    Where
+                        Nome = @Nome";
+
+        var retorno = await ExecuteQueryAsync
+            <PessoaEntity>(sql, parametros);
+
+        return retorno.ToList();
+    }
+}
--- src/Cepedi.BancoCentral.PagamentoPix.IoC/IoCServiceExtension.cs
+++ src/Cepedi.BancoCentral.PagamentoPix.IoC/IoCServiceExtension.cs
@@ -1,13 +1,19 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Cepedi.BancoCentral.PagamentoPix.Cache;
 using Cepedi.BancoCentral.PagamentoPix.Compartilhado;
 using Cepedi.BancoCentral.PagamentoPix.Dados.Repositorios;
+using Cepedi.BancoCentral.PagamentoPix.Dados.Repositorios.Queries;
 using Cepedi.BancoCentral.PagamentoPix.Data;
 using Cepedi.BancoCentral.PagamentoPix.Data.Repositories;
 using Cepedi.BancoCentral.PagamentoPix.Dominio.Handlers.Pipelines;
 using Cepedi.BancoCentral.PagamentoPix.Dominio.Repositorio;
+using Cepedi.BancoCentral.PagamentoPix.Dominio.Repositorio.Queries;
+using Cepedi.BancoCentral.PagamentoPix.Dominio.Servicos;
 using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -34,6 +40,19 @@ namespace Cepedi.BancoCentral.PagamentoPix.IoC
             services.AddScoped<IPixRepository, PixRepository>();
             services.AddScoped<ITransacaoPixRepository, TransacaoPixRepository>();
             services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IPessoaQueryRepository, PessoaQueryRepository>();
+
+            // Cache Redis
+            services.AddStackExchangeRedisCache(obj =>
+            {
+                obj.Configuration = "stringConff";
+                obj.InstanceName = "BancoCentral.PagamentoPix";
+            });
+
+            services.AddSingleton<IDistributedCache, RedisCache>();
+            services.AddScoped(typeof(ICache<>), typeof(Cache<>));
+            // CacheRedis
         
             // services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddHealthChecks()
