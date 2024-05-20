f8af07125a8252e37b3ef8590c42d3a6d5e1f1cf
Ana Caroline <carolnerisfonseca@gmail.com>
2024-05-19 21:20:46 -0300

    validator criar conta

---
+++ src/Cepedi.BancoCentral.PagamentoPix.Compartilhado/Validators/Conta/CriarContaResquestValidator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Requests;
+using FluentValidation;
+
+namespace Cepedi.BancoCentral.PagamentoPix.Compartilhado.Validators
+{
+    public class CriarContaRequestValidator : AbstractValidator<CriarContaRequest>
+    {
+        public CriarContaRequestValidator()
+        {
+            
+             RuleFor(request => request.IdPessoa)
+            .NotNull().WithMessage("IdPessoa não pode ser nulo.");
+
+        RuleFor(request => request.Numero)
+            .NotEmpty().WithMessage("Número da instituição é obrigatório.");
+
+        RuleFor(request => request.Agencia)
+            .NotEmpty().WithMessage("Agência é obrigatória.")
+            .Matches(@"^\d+$").WithMessage("A agência deve ser em formato numérico.");
+
+        RuleFor(request => request.Conta)
+            .NotEmpty().WithMessage("Conta é obrigatória.")
+            .Matches(@"^\d+$").WithMessage("A conta deve ser em formato numérico.");
+        }
+    }
+}
