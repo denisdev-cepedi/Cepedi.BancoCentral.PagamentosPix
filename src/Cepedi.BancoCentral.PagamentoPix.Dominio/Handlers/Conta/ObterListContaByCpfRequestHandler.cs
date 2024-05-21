4dd74cdf505ea5947dc8be9d80031033efae92b7
Ana Caroline <carolnerisfonseca@gmail.com>
2024-05-21 08:39:25 -0300

    obter conta cpf

--- src/Cepedi.BancoCentral.PagamentoPix.Dominio/Handlers/Conta/ObterListContaByCpfRequestHandler.cs
+++ src/Cepedi.BancoCentral.PagamentoPix.Dominio/Handlers/Conta/ObterListContaByCpfRequestHandler.cs
@@ -1,6 +1,47 @@
-﻿namespace Cepedi.BancoCentral.PagamentoPix.Dominio;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Requests;
+using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Responses;
+using Cepedi.BancoCentral.PagamentoPix.Dominio.Repositorio;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
 
-public class ObterListContaByCpfRequestHandler
+namespace Cepedi.BancoCentral.PagamentoPix.Dominio.Handlers
 {
+    public class ObterListContaByCpfRequestHandler :
+        IRequestHandler<ObterListContaByCpfRequest, Result<ObterListContaByCpfResponse>>
+    {
+        private readonly ILogger<ObterListContaByCpfRequestHandler> _logger;
+        private readonly IContaRepository _contaRepositorio;
 
+        public ObterListContaByCpfRequestHandler(
+            ILogger<ObterListContaByCpfRequestHandler> logger,
+            IContaRepository contaRepositorio)
+            {
+                _logger = logger;
+                _contaRepositorio = contaRepositorio;
+            }
+
+        public async Task<Result<ObterListContaByCpfResponse>> Handle(ObterListContaByCpfRequest request, CancellationToken cancellationToken)
+        {
+            var contas = await _contaRepositorio.ObtemContasAsync(request.Cpf);
+
+            var response = new ObterListContaByCpfResponse()
+            {
+                Contas = contas.Select(c => new ObterContaResponse()
+                {
+                    IdConta = c.IdConta,
+                    IdPessoa = c.IdPessoa,
+                    Numero = c.Numero,
+                    Conta = c.Conta,
+                    Agencia = c.Agencia
+                }).ToList()
+            };
+            return Result.Success(response);
+
+        }
+    }
 }
