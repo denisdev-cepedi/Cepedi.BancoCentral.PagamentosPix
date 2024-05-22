17904a005b7f4da58a9cfe963ca56aacca2c4f8a
Lucas Silva <lucas_avlis@outlook.com>
2024-05-22 15:54:59 -0300

    Adicionando configuração Elastic e Kibana

--- src/Cepedi.BancoCentral.PagamentoPix.Api/Program.cs
+++ src/Cepedi.BancoCentral.PagamentoPix.Api/Program.cs
@@ -1,6 +1,8 @@
 using Serilog;
 using Cepedi.BancoCentral.PagamentoPix.IoC;
 using Cepedi.BancoCentral.PagamentoPix.Api;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,9 +18,16 @@ builder.Services.ConfigureAppDependencies(builder.Configuration);
 
 builder.Host.UseSerilog((context, configuration) =>
 {
-    configuration.ReadFrom.Configuration(context.Configuration);
+    configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithExceptionDetails()
+    .Enrich.WithProperty("Environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!)
+    .WriteTo.Elasticsearch(ConfigureElasticSink(context.Configuration, Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!));
 });
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +35,16 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+static ElasticsearchSinkOptions ConfigureElasticSink(IConfiguration configuration, string environment)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"pagamentospixlucas{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+        //IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+    };
 }
 
 app.UseHealthChecks("/health");
