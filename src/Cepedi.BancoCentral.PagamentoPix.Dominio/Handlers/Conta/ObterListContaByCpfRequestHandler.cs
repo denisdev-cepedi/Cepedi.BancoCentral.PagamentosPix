4a51e52385fbc460d2a17ffa60c8b5e19f4eee03
Ana Caroline <carolnerisfonseca@gmail.com>
2024-05-21 22:11:14 -0300

    Rodando o get contas por cpf, porém, capo duplicado

--- src/Cepedi.BancoCentral.PagamentoPix.Dominio/Handlers/Conta/ObterListContaByCpfRequestHandler.cs
+++ src/Cepedi.BancoCentral.PagamentoPix.Dominio/Handlers/Conta/ObterListContaByCpfRequestHandler.cs
@@ -1,63 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
-using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Excecoes;
+using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Enums;
 using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Requests;
 using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Responses;
+using Cepedi.BancoCentral.PagamentoPix.Dominio.Entidades;
 using Cepedi.BancoCentral.PagamentoPix.Dominio.Repositorio;
-
+using Cepedi.BancoCentral.PagamentoPix.Compartilhado.Excecoes;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using OperationResult;
 
 namespace Cepedi.BancoCentral.PagamentoPix.Dominio.Handlers
 {
-    public class ObterListContaByCpfRequestHandler : IRequestHandler<ObterListContaByCpfRequest, Result<ObterListContaByCpfResponse>>
+    public class ObterListContaByCpfRequestHandler : IRequestHandler<ObterListContaByCpfRequest, 
+    Result<ObterListContaByCpfResponse>>
     {
         private readonly IContaRepository _contaRepository;
-
         private readonly IPessoaRepository _pessoaRepository;
         private readonly ILogger<ObterListContaByCpfRequestHandler> _logger;
 
-
-        public ObterListContaByCpfRequestHandler(IContaRepository contaRepository, ILogger<ObterListContaByCpfRequestHandler> logger, IPessoaRepository pessoaRepository)
+        public ObterListContaByCpfRequestHandler(IContaRepository contaRepository, 
+        ILogger<ObterListContaByCpfRequestHandler> logger, IPessoaRepository pessoaRepository)
         {
             _contaRepository = contaRepository;
             _logger = logger;
             _pessoaRepository = pessoaRepository;
-            
         }
 
-        
+        public async Task<Result<ObterListContaByCpfResponse>> 
+        Handle(ObterListContaByCpfRequest request, CancellationToken cancellationToken)
+        {
+            var pessoa = await _pessoaRepository.ObtemPessoaPorCpfAsync(request.Cpf);
 
-        public async Task<Result<ObterListContaByCpfResponse>> Handle(ObterListContaByCpfRequest request, CancellationToken cancellationToken)
-{
-    var pessoa = await _pessoaRepository.ObtemPessoaPorCpfAsync(request.Cpf);
-
-
-    if ( pessoa == null)
-    {
-        _logger.LogError("CPF não encontrado");
-        return Result.Error<CriarContaResponse>(
-            new Compartilhado.Excecoes.ExcecaoAplicacao(
-                        (PagamentosPix.PessoaInexistente)
-                    ));
-    }
-
-            var response = new ObterListContaByCpfResponse()
+           if (pessoa == null)
             {
-                Contas = pessoa.Select(c => new ObterPessoaResponse()
+                _logger.LogError("CPF não encontrado");
+                return Result.Error<ObterListContaByCpfResponse>(
+                    new ExcecaoAplicacao(PagamentosPix.PessoaInexistente));
+            }
+
+            var contas = await _contaRepository.ObtemContasAsync(pessoa.IdPessoa);
+
+            var response = new ObterListContaByCpfResponse
+            {
+                Contas = contas.Select(conta => new ObterContaResponse
                 {
-                    IdConta = c.IdConta,
-                    IdPessoa = c.IdPessoa,
-                    Numero = c.Numero,
-                    Conta = c.Conta,
-                    Agencia = c.Agencia
+                    IdConta = conta.IdConta,
+                    IdPessoa = conta.IdPessoa,
+                    Numero = conta.Numero,
+                    Conta = conta.Conta,
+                    Agencia = conta.Agencia
                 }).ToList()
             };
-            return Result.Success(response);
 
+            return Result.Success(response);
         }
     }
 }
